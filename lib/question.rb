662b1d5fd2d258c9de8f56894ab544db7af2fd8e
William Poynter <poynter.william@gmail.com>
2015-12-03 22:40:33 +0000

    Fixed polymorphic reversal

--- lib/question.rb
+++ lib/question.rb
@@ -2,11 +2,11 @@ module Question
   extend ActiveSupport::Concern
   included do
     belongs_to :instruction
-    has_many :rds_qs, class_name: 'RdsQs', as: :response_domain
-    has_many :response_domain_codes, through: :rds_qs, source: :question, source_type: 'ResponseDomainCode'
-    has_many :response_domain_datetimes, through: :rds_qs, source: :question, source_type: 'ResponseDomainDatetime'
-    has_many :response_domain_numerics, through: :rds_qs, source: :question, source_type: 'ResponseDomainNumeric'
-    has_many :response_domain_texts, through: :rds_qs, source: :question, source_type: 'ResponseDomainText'
+    has_many :rds_qs, class_name: 'RdsQs', as: :question, dependent: :destroy
+    has_many :response_domain_codes, through: :rds_qs, source: :response_domain, source_type: 'ResponseDomainCode'
+    has_many :response_domain_datetimes, through: :rds_qs, source: :response_domain, source_type: 'ResponseDomainDatetime'
+    has_many :response_domain_numerics, through: :rds_qs, source: :response_domain, source_type: 'ResponseDomainNumeric'
+    has_many :response_domain_texts, through: :rds_qs, source: :response_domain, source_type: 'ResponseDomainText'
   
     def response_domains
       self.response_domain_codes.to_a + self.response_domain_datetimes.to_a + 
--- lib/response_domain.rb
+++ lib/response_domain.rb
@@ -1,9 +1,9 @@
 module ResponseDomain
   extend ActiveSupport::Concern
   included do
-    has_many :rds_qs, class_name: 'RdsQs', as: :question
-    has_many :question_items, through: :rds_qs, source: :response_domain, source_type: 'QuestionItem'
-    has_many :question_grids, through: :rds_qs, source: :response_domain, source_type: 'QuestionGrid'
+    has_many :rds_qs, class_name: 'RdsQs', as: :response_domain, dependent: :destroy
+    has_many :question_items, through: :rds_qs, source: :question, source_type: 'QuestionItem'
+    has_many :question_grids, through: :rds_qs, source: :question, source_type: 'QuestionGrid'
 
     def questions
       self.question_items.to_a + self.question_grids.to_a
