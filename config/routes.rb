46d05f5ae6a97572566ec790665f6e83a5a28614
William Poynter <poynter.william@gmail.com>
2015-12-07 17:39:18 +0000

    Added a static home page

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   resources :codes
   resources :code_lists
   resources :categories
+
+  root 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
