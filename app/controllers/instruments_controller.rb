66703e9a8b0be7c353d33cca61c81969984b3229
William Poynter <poynter.william@gmail.com>
2016-01-15 13:18:21 +0000

    Create tmp uploads folder during import

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -16,6 +16,7 @@ class InstrumentsController < ApplicationController
   end
 
   def import
+    FileUtils.mkdir_p Rails.root.join('tmp','uploads')
     params[:files].each do |file|
       filepath = Rails.root.join(
           'tmp',
