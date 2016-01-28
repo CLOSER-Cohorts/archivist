a6ad271fcf6de3763e3e441483935cd3463e7091
William Poynter <poynter.william@gmail.com>
2016-01-28 18:48:50 +0000

    Fixed stupid mistype bug

--- lib/realtime.rb
+++ lib/realtime.rb
@@ -9,7 +9,7 @@ module Realtime
       output[:data] = self.to_json except: :created_at
       output[:data] = JSON.parse output[:data]
       output[:data][:type] = self.class.name
-      $redis.publish 'rt-update', output[:data].to_json
+      $redis.publish 'rt-update', output.to_json
     end
   end
 end
