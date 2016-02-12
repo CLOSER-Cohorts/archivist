5aed81ada531d74e978a3118f5a1cca0ee2088dc
wpoynter <poynter.william@gmail.com>
2016-02-12 12:34:45 +0000

    Added documentation

--- lib/realtime.rb
+++ lib/realtime.rb
@@ -1,3 +1,20 @@
+# The Realtime module connects a model to Redis via the RT Update channel
+#
+# By including the Realtime module in model all commits to the database made
+# by this model will publish an update to Redis on the RT Update channel. The
+# practical upshot of this is that realtime updates can be publish to all
+# current Archivist users so there is no need to manually refresh their browers.
+#
+# ==== Use by
+#
+#   include Realtime
+#
+# === Todo
+#
+# * Allow for options to exclude certain database actions
+# * Allow for options to alter the output packet before publishing
+# * Potentially provide option for pre/post publish filter with stopping option
+#
 module Realtime
   extend ActiveSupport::Concern
   included do
