1bbe7e7de83a17d368a1216b5d255bc06ab5e06d
wpoynter <poynter.william@gmail.com>
2016-02-12 12:10:01 +0000

    Removed unused lines and documented the rest

--- lib/realtime.rb
+++ lib/realtime.rb
@@ -6,11 +6,15 @@ module Realtime
     private
     def rt_update
       output = {}
+      #Create JSON string from model
       data = self.to_json except: :created_at
-      output[:data] = self.to_json except: :created_at
+      #Parse it back to a model
       data = JSON.parse data
+      #Attach the Class as type
       data[:type] = self.class.name
+      #Place the data in the output object
       output[:data] = [data]
+      #Publish the output as JSON to the rt-update channel
       $redis.publish 'rt-update', output.to_json
     end
   end
