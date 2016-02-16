80f3f43c80643573c67a5b016d4e8b29988d8142
wpoynter <poynter.william@gmail.com>
2016-02-16 11:15:05 +0000

    Further code improvement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   after_filter :set_csrf_cookie_for_ng
 
