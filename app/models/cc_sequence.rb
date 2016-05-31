f204f20cc1003b46f4975fa32952e84c45c70027
wpoynter <poynter.william@gmail.com>
2016-05-31 12:39:01 +0100

    DRYed construct model code

--- app/models/cc_sequence.rb
+++ app/models/cc_sequence.rb
@@ -6,25 +6,7 @@ class CcSequence < ActiveRecord::Base
   TYPE = 'Sequence'
 
   def self.create_with_position(params)
-    obj = new()
-    i = Instrument.find(params[:instrument_id])
-    i.send('cc_' + params[:type].pluralize) << obj
-
-    parent = i.send('cc_' + params[:parent][:type].pluralize).find(params[:parent][:id])
-    unless parent.nil?
-      if parent.has_children?
-        obj.position = parent.last_child.position + 1
-      else
-        obj.position = 1
-      end
-
-      unless params[:branch].nil?
-        obj.branch = params[:branch]
-      end
+    super do |obj|
     end
-    obj.label = params[:label]
-    parent.children << obj.cc
-    obj.save!
-    obj
   end
 end
--- lib/construct.rb
+++ lib/construct.rb
@@ -59,6 +59,32 @@ module Construct::Model
           .where('label = ?', label)
           .first
     end
+
+    def create_with_position(params)
+      obj = new()
+      i = Instrument.find(params[:instrument_id])
+      i.send('cc_' + params[:type].pluralize) << obj
+
+      parent = i.send('cc_' + params[:parent][:type].pluralize).find(params[:parent][:id])
+      unless parent.nil?
+        if parent.has_children?
+          obj.position = parent.last_child.position + 1
+        else
+          obj.position = 1
+        end
+
+        unless params[:branch].nil?
+          obj.branch = params[:branch]
+        end
+      end
+      obj.label = params[:label]
+
+      yield obj
+
+      parent.children << obj.cc
+      obj.save!
+      obj
+    end
   end
 
   module LocalInstanceMethods
