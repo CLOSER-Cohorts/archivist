b6fb2cbf0e1fb9f299ab21f03e16fc91ff9da400
wpoynter <poynter.william@gmail.com>
2016-06-03 12:46:30 +0100

    Added confirmable and lockable

---
+++ db/migrate/20160603113436_add_confirmable_and_lockable.rb
@@ -0,0 +1,15 @@
+class AddConfirmableAndLockable < ActiveRecord::Migration
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+
+    add_column :users, :failed_attempts, :integer
+    add_column :users, :unlock_token, :string
+    add_column :users, :locked_at, :datetime
+
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
+  end
+end
