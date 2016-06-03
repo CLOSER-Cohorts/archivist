218f9b45fdb5f4d92963d1216a25393746a96304
wpoynter <poynter.william@gmail.com>
2016-06-03 16:17:44 +0100

    Added missing route for confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
       sessions: 'users/sessions',
-#      confirmations: 'users/confirmations',
+      confirmations: 'users/confirmations',
 #      omiauth: 'users/omiauth',
       passwords: 'users/passwords',
       registrations: 'users/registrations',
