47ee32395400e19bed742852c2cc1a17259f39a0
wpoynter <poynter.william@gmail.com>
2016-08-12 11:31:52 +0100

    Reinstated autoloading for production

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module Archivist
 
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.enable_dependency_loading = true
+
     config.action_mailer.delivery_method = :postmark
     config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_KEY'] }
     config.action_mailer.default_url_options = {host: (ENV['HOSTNAME'] || 'localhost')}
