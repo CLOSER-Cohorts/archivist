a02c47965b9a3cb1e8b7f3964a75f260300d43e1
wpoynter <poynter.william@gmail.com>
2016-08-13 15:16:25 +0100

    Added export admin page

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -2,16 +2,14 @@ class ExportJob
   @queue = :in_and_out
 
   def self.perform id
-    puts "Export started"
     begin
       exp = XML::DDI::Exporter.new
       exp.add_root_attributes
       filename = exp.run Instrument.find(id)
       obj = $s3_bucket.object filename.basename.to_s
       obj.upload_file filename.to_s, acl:'public-read'
-      puts "Export completed"
+      $redis.hset 'export:instruments', id, obj.public_url.to_s
     rescue => e
-      puts e
       Rails.logger.fatal e
     end
   end
