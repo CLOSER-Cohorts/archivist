17c965c407379df86881ad964b9a2d278873fd9d
wpoynter <poynter.william@gmail.com>
2016-08-13 11:29:41 +0100

    Added debugging code

--- app/jobs/import.rb
+++ app/jobs/import.rb
@@ -3,10 +3,13 @@ class Import
 
   def self.perform filepath
     begin
+      puts 'Starting importer'
       im = XML::CADDIES::Importer.new filepath
       im.parse
+      puts 'Finished importer'
     rescue => e
       Rails.logger.fatal e
+      puts 'Failed to import'
     end
   end
 end
