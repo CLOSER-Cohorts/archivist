4d09a1a2b69a6d051aa0445e8773f2b1af0ef720
wpoynter <poynter.william@gmail.com>
2016-08-13 12:03:50 +0100

    Added debugging code

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -56,7 +56,7 @@ class InstrumentsController < ApplicationController
         f.write(file.read)
       end
       begin
-        Resque.enqueue Import, filepath[:path]
+        Resque.enqueue Import, filepath.to_s
         head :ok, format: :json
       rescue  => e
         render json: {message: e}, status: :bad_request
