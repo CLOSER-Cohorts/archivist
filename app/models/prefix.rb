958a66acfba59dd73e6a7ce39a124fb0af294c98
William Poynter <Will Poynter>
2016-11-17 09:59:52 +0000

    Made prefix model not error if there is no redis conenction

--- app/models/prefix.rb
+++ app/models/prefix.rb
@@ -1,19 +1,35 @@
 class Prefix
   class << self
     def [](prefix)
-      if (id = redis.hget hash, prefix).nil?
-        prefix
-      else
-        id
+      begin
+        if (id = redis.hget hash, prefix).nil?
+          prefix
+        else
+          id
+        end
+      rescue Exception => e
+        logger.warn 'Unable to get prefix from Redis cache. Returning input.'
+        logger.warn e.message
+        return id
       end
     end
 
     def []=(prefix, id)
-      redis.hset hash, prefix, id
+      begin
+        redis.hset hash, prefix, id
+      rescue Exception => e
+        logger.warn 'Unable to save prefix to Redis cache'
+        logger.warn e.message
+      end
     end
 
     def destroy(prefix)
-      redis.hdel hash, prefix
+      begin
+        redis.hdel hash, prefix
+      rescue Exception => e
+        logger.warn 'Unable to delete prefix from Redis cache'
+        logger.warn e.message
+      end
     end
 
     private
