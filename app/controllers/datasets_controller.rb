1c2b43c6d3f1ae11b780193102659ac37f0c9b9b
Will Poynter <poynter.william@gmail.com>
2016-11-18 10:15:50 +0000

    Moved dataset importing to a job

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -3,4 +3,20 @@ class DatasetsController < BasicController
 
   @model_class = Dataset
   @params_list = [:name]
+
+  def import
+    files = params[:files].nil? ? [] : params[:files]
+    options = {}
+    head :ok, format: :json if files.empty?
+    begin
+      files.each do |file|
+        doc = Document.new file: file
+        doc.save_or_get
+        Resque.enqueue ImportJob, doc.id, options
+      end
+      head :ok, format: :json
+    rescue  => e
+      render json: {message: e}, status: :bad_request
+    end
+  end
 end
