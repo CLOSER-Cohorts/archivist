59a5977ce0bb0ac19c1d6d61d527cdde7766a5d3
William Poynter <Will Poynter>
2017-04-18 14:23:21 +0100

    Uncommenting out member_imports

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -94,26 +94,26 @@ class InstrumentsController < BasicController
   # Please note that for multiple upload to work within a nested
   # Angular 1.X form, base64 encoding was needed so we need to
   # decode the file here as well
-  # def member_imports
-  #   imports = params[:imports].nil? ? [] : params[:imports]
-  #   head :ok, format: :json if imports.empty?
-  #
-  #   binding.pry
-  #   1
-  #   begin
-  #     imports.each do |import|
-  #       doc = Document.new file: Base64.decode64(import[:file])
-  #       doc.save_or_get
-  #
-  #       type = import[:type]&.downcase&.to_sym
-  #
-  #       Resque.enqueue(@@map[type], doc.id, @object)
-  #     end
-  #     head :ok, format: :json
-  #   rescue  => e
-  #     render json: {message: e}, status: :bad_request
-  #   end
-  # end
+  def member_imports
+    imports = params[:imports].nil? ? [] : params[:imports]
+    head :ok, format: :json if imports.empty?
+    #
+    # binding.pry
+    # 1
+    begin
+      imports.each do |import|
+        doc = Document.new file: Base64.decode64(import[:file])
+        doc.save_or_get
+
+        type = import[:type]&.downcase&.to_sym
+
+        Resque.enqueue(@@map[type], doc.id, params[:id])
+      end
+      head :ok, format: :json
+    rescue  => e
+      render json: {message: e}, status: :bad_request
+    end
+  end
 
   def copy
     new_details = params.select do |k|
