597af3ac67f14ee2bbf05b94db2cb0784a5c2e94
Will Poynter <poynter.william@gmail.com>
2017-08-09 10:15:35 +0100

    Added Pundit to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
   protect_from_forgery with: :exception
 
   after_action :set_csrf_cookie_for_ng
