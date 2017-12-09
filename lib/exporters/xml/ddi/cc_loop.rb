c7b685c83d76e435d0aef49684f1750d17aa8c79
William Poynter <Will Poynter>
2017-12-09 17:55:40 +0000

    Refactored code

--- lib/exporters/xml/ddi/cc_loop.rb
+++ lib/exporters/xml/ddi/cc_loop.rb
@@ -20,11 +20,7 @@ module Exporters::XML::DDI
     # @param [::CcLoop|Integer] lp_id Either the CcLoop or CcLoop ID for exporting
     # @return [Nokogiri::XML::Node] New XML node
     def V3_2(lp_id)
-      if lp_id.is_a? ::CcLoop
-        cc = lp_id
-      else
-        cc = ::CcLoop.find lp_id
-      end
+      cc = lp_id.is_a? ::CcLoop ? lp_id : ::CcLoop.find(lp_id)
 
       lp = Nokogiri::XML::Node.new 'd:Loop', @doc
       urn = create_urn_node cc
@@ -39,6 +35,18 @@ module Exporters::XML::DDI
                        '<r:CommandContent>%{command}</r:CommandContent></r:Command></d:InitialValue>' % {
                            command: CGI::escapeHTML(cc.loop_var.to_s) + ' = ' + cc.start_val
                        }
+
+      lp.add_child build_loop_while(cc)
+      lp.add_child build_loop_sequence_reference(cc)
+
+      ns = Nokogiri::XML::NodeSet.new @doc
+      ns.push lp
+      ns.push build_loop_sequence(cc.children)
+      ns
+    end
+
+    private # Private methods
+    def build_loop_while(cc)
       command = ''
       unless cc.end_val.nil?
         command += cc.loop_var + ' < ' + cc.end_val
@@ -50,17 +58,22 @@ module Exporters::XML::DDI
         command += cc.loop_while
       end
 
-      lp.add_child '<d:LoopWhile><r:Command><r:ProgramLanguage>pseudo-code</r:ProgramLanguage>' +
+      return '<d:LoopWhile><r:Command><r:ProgramLanguage>pseudo-code</r:ProgramLanguage>' +
                        '<r:CommandContent>%{command}</r:CommandContent></r:Command></d:LoopWhile>' % {
                            command: CGI::escapeHTML(command.to_s)
                        }
-      lp.add_child ('<d:ControlConstructReference><r:URN>urn:ddi:%{agency}:%{prefix}-selp-%{id}:1.0.0</r:URN>' +
-          '<r:TypeOfObject>Sequence</r:TypeOfObject></d:ControlConstructReference>') % {
-            agency: cc.instrument.agency,
-            prefix: cc.instrument.prefix,
-            id: '%06d' % cc.id
-        }
+    end
 
+    def build_loop_sequence_reference(cc)
+      ('<d:ControlConstructReference><r:URN>urn:ddi:%{agency}:%{prefix}-selp-%{id}:1.0.0</r:URN>' +
+          '<r:TypeOfObject>Sequence</r:TypeOfObject></d:ControlConstructReference>') % {
+          agency: cc.instrument.agency,
+          prefix: cc.instrument.prefix,
+          id: '%06d' % cc.id
+      }
+    end
+
+    def build_loop_sequence(children)
       selp = Nokogiri::XML::Node.new 'd:Sequence', @doc
       selp.add_child create_urn_node(cc, 'selp')
       selp_cn = Nokogiri::XML::Node.new 'd:ConstructName', @doc
@@ -78,16 +91,12 @@ module Exporters::XML::DDI
       selp_cn.add_next_sibling selp_l
 
       selp_inner_prev = selp_l
-      cc.children.each do |child|
+      children.each do |child|
         ccf = create_reference_string 'd:ControlConstructReference', child.construct
         selp_inner_prev.add_next_sibling ccf
         selp_inner_prev = ccf
       end
-
-      ns = Nokogiri::XML::NodeSet.new @doc
-      ns.push lp
-      ns.push selp
-      ns
+      return selp
     end
   end
 end
