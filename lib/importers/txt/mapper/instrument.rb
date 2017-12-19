4aa7c178100cbedcbd40cab6740a19238581518a
Will Poynter <poynter.william@gmail.com>
2017-12-19 16:59:59 +0000

    Debugged QV importer

--- lib/importers/txt/mapper/instrument.rb
+++ lib/importers/txt/mapper/instrument.rb
@@ -1,6 +1,6 @@
 class Importers::TXT::Mapper::Instrument < Importers::TXT::Basic
   def initialize(thing, options)
-    super do |instrument_id|
+    super(thing, options) do |instrument_id|
       Instrument.find instrument_id
     end
   end
