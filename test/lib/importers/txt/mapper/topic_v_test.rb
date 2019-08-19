cf68e74263bf559c79719c4217317f481ccfa57a
Simon Reed <hello@simonreed.co>
2019-08-19 15:57:07 +0100

    Fix failing test

--- test/lib/importers/txt/mapper/topic_v_test.rb
+++ test/lib/importers/txt/mapper/topic_v_test.rb
@@ -21,7 +21,7 @@ class Importers::TXT::Mapper::TopicVTest < ActiveSupport::TestCase
       it "should create a new mappings for the topic and variables" do
         Link.create(target_type: 'Variable', target_id: 156092, topic_id: 2)
         assert_equal('102', @dataset.variables.find_by_name('kw0001').fully_resolved_topic_code)
-        new_txt = "aln\t10320\nqlet\t10320\n"
+        new_txt = "aln\t10320\nkw0001\t10320\n"
         doc = Document.create(file: new_txt, item: @dataset)
         Importers::TXT::Mapper::TopicV.new(doc.id, {:object=>@dataset.id.to_s}).import
         assert_equal('10320', @dataset.variables.find_by_name('kw0001').fully_resolved_topic_code)
