90cdef0c54b59c6f8f57a2813f3c83d5099f83c3
Simon Reed <hello@simonreed.co>
2019-09-03 17:00:05 +0100

    Fix failing migration on staging

--- db/migrate/20190813092806_add_slug_to_instruments.rb
+++ db/migrate/20190813092806_add_slug_to_instruments.rb
@@ -1,7 +1,9 @@
 class AddSlugToInstruments < ActiveRecord::Migration[5.0]
   def change
-    add_column :instruments, :slug, :string
-    add_index :instruments, :slug, unique: true
+    unless column_exists? :instruments, :slug
+      add_column :instruments, :slug, :string
+      add_index :instruments, :slug, unique: true
+    end
 
     Instrument.find_each{|i| i.update_column(:slug, i.prefix)}
   end
