345941310a1edbe82767cae01851089352aa7a3d
Simon Reed <hello@simonreed.co>
2019-09-26 10:00:34 +0100

    Handle an exception being raised on a import line elegantly

--- lib/importers/txt/mapper/dv.rb
+++ lib/importers/txt/mapper/dv.rb
@@ -5,31 +5,31 @@ class Importers::TXT::Mapper::DV < Importers::TXT::Mapper::Dataset
     begin
         @doc.each do |variable_dataset, v, source_dataset, s|
           log :input, "#{variable_dataset},#{v},#{source_dataset},#{s}"
-          if variable_dataset.blank? || source_dataset.blank? || v.blank? || s.blank?
-            @errors = true
-            log :outcome, I18n.t('importers.txt.mapper.dv.wrong_number_of_columns', actual_number_of_columns: {a: source_dataset, b: variable_dataset, c: v, d: s}.compact.count)
-            write_to_log
-            next
-          elsif variable_dataset != @object.instance_name
-            @errors = true
-            log :outcome, I18n.t('importers.txt.mapper.dv.record_invalid_dataset', dataset_from_line: variable_dataset, dataset_from_object: @object.instance_name)
-            write_to_log
-            next
-          end
-          var = vars.find_by_name v
-          src = vars.find_by_name s
-          log :matches, "matched to Variable (#{var}) AND Source (#{src})"
-          unless var.nil? or src.nil?
-            var.var_type = 'Derived'
-            var.src_variables << src
-            if var.save
-              log :outcome, "Record Saved"
-            else
-              errors = true
-              log :outcome, "Record Invalid : #{var.errors.full_messages.to_sentence}"
+          begin
+            if variable_dataset.blank? || source_dataset.blank? || v.blank? || s.blank?
+              raise StandardError.new(I18n.t('importers.txt.mapper.dv.wrong_number_of_columns', actual_number_of_columns: {a: source_dataset, b: variable_dataset, c: v, d: s}.compact.count))
+            elsif variable_dataset != @object.instance_name
+              raise StandardError.new(I18n.t('importers.txt.mapper.dv.record_invalid_dataset', dataset_from_line: variable_dataset, dataset_from_object: @object.instance_name))
             end
+            var = vars.find_by_name v
+            src = vars.find_by_name s
+            log :matches, "matched to Variable (#{var}) AND Source (#{src})"
+            unless var.nil? or src.nil?
+              var.var_type = 'Derived'
+              var.src_variables << src
+              if var.save
+                log :outcome, "Record Saved"
+              else
+                errors = true
+                log :outcome, "Record Invalid : #{var.errors.full_messages.to_sentence}"
+              end
+            end
+          rescue StandardError => e
+            @errors = true
+            log :outcome, (e.message =~ /Record Invalid/) ? e.message : "Record Invalid : #{e.message}"
+          ensure
+            write_to_log
           end
-          write_to_log
         end
     rescue => exception
       Rails.logger.info exception.message
--- lib/importers/txt/mapper/topic_q.rb
+++ lib/importers/txt/mapper/topic_q.rb
@@ -4,34 +4,34 @@ class Importers::TXT::Mapper::TopicQ < Importers::TXT::Mapper::Instrument
     set_import_to_running
     @doc.each do |control_construct_scheme, q, t|
       log :input, "#{control_construct_scheme},#{q},#{t}"
-      if control_construct_scheme.blank? || q.blank? || t.blank?
-        @errors = true
-        log :outcome, I18n.t('importers.txt.mapper.topic_q.wrong_number_of_columns', actual_number_of_columns: {a: control_construct_scheme, b: q, c: t}.compact.count)
-        write_to_log
-        next
-      elsif control_construct_scheme != @object.control_construct_scheme
-        @errors = true
-        log :outcome, I18n.t('importers.txt.mapper.topic_q.record_invalid_control_construct_scheme', control_construct_scheme_from_line: control_construct_scheme, control_construct_scheme_from_object: @object.control_construct_scheme)
-        write_to_log
-        next
-      end
-      qc = @object.cc_questions.find_by_label q
-      topic = Topic.find_by_code t
-      log :matches, "matched to QuestionContruct(#{qc}) AND Topic (#{topic})"
-      if qc.nil? || topic.nil?
-        @errors = true
-        log :outcome, "Record Invalid as QuestionContruct and Topic where not found"
-      else
-        qc.topic = topic
-        if qc.save
-          log :outcome, "Record Saved"
-          cc_question_ids_to_delete.delete(qc.id)
-        else
-          @errors = true
-          log :outcome, "Record Invalid : #{qc.errors.full_messages.to_sentence}"
+      begin
+        if control_construct_scheme.blank? || q.blank? || t.blank?
+          raise StandardError.new(I18n.t('importers.txt.mapper.topic_q.wrong_number_of_columns', actual_number_of_columns: {a: control_construct_scheme, b: q, c: t}.compact.count))
+        elsif control_construct_scheme != @object.control_construct_scheme
+          raise StandardError.new(I18n.t('importers.txt.mapper.topic_q.record_invalid_control_construct_scheme', control_construct_scheme_from_line: control_construct_scheme, control_construct_scheme_from_object: @object.control_construct_scheme))
         end
+        qc = @object.cc_questions.find_by_label q
+        topic = Topic.find_by_code t
+        log :matches, "matched to QuestionContruct(#{qc}) AND Topic (#{topic})"
+        if qc.nil? || topic.nil?
+          @errors = true
+          log :outcome, "Record Invalid as QuestionContruct and Topic where not found"
+        else
+          qc.topic = topic
+          if qc.save
+            log :outcome, "Record Saved"
+            cc_question_ids_to_delete.delete(qc.id)
+          else
+            @errors = true
+            log :outcome, "Record Invalid : #{qc.errors.full_messages.to_sentence}"
+          end
+        end
+      rescue StandardError => e
+        @errors = true
+        log :outcome, (e.message =~ /Record Invalid/) ? e.message : "Record Invalid : #{e.message}"
+      ensure
+        write_to_log
       end
-      write_to_log
     end
     Link.where(target_type: 'CcQuestion', target_id: cc_question_ids_to_delete).delete_all
     set_import_to_finished
