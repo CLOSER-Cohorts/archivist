6c9dbbcc2e6d9c0c8c1332b4a2c2348e3bcdd3dc
Simon Reed <hello@simonreed.co>
2020-10-22 13:28:08 +0100

    Fix migration that was causing instruments page to fail

--- db/migrate/20201021193720_add_signed_off_to_instruments.rb
+++ db/migrate/20201021193720_add_signed_off_to_instruments.rb
@@ -1,7 +1,7 @@
 class AddSignedOffToInstruments < ActiveRecord::Migration[5.2]
   def change
     unless column_exists? :instruments, :signed_off
-      add_column :instruments, :signed_off, default: false
+      add_column :instruments, :signed_off, :boolean, default: false
     end
   end
 end
