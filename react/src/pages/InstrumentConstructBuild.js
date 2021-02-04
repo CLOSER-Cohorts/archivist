ff4e085f069ecdbd2369b8d5058bc21f44663312
Simon Reed <hello@simonreed.co>
2021-02-04 14:44:42 +0000

    Construct search should go to result

--- react/src/pages/InstrumentConstructBuild.js
+++ react/src/pages/InstrumentConstructBuild.js
@@ -209,7 +209,6 @@ const Tree = (props) => {
             }
       onCancelSearch={() => {
               setSearchString()
-              setSearchFocusIndex(0)
             }}
     />
 
@@ -250,25 +249,9 @@ const Tree = (props) => {
         canDrag={({node}) =>{
           return !['conditionTrue', 'conditionFalse'].includes(node.type)
         }}
-        //
-        // Custom comparison for matching during search.
-        // This is optional, and defaults to a case sensitive search of
-        // the title and subtitle values.
-        // see `defaultSearchMethod` in https://github.com/frontend-collective/react-sortable-tree/blob/master/src/utils/default-handlers.js
         searchMethod={customSearchMethod}
-        //
-        // The query string used in the search. This is required for searching.
         searchQuery={searchString}
-        //
-        // When matches are found, this property lets you highlight a specific
-        // match and scroll to it. This is optional.
         searchFocusOffset={searchFocusIndex}
-        //
-        // This callback returns the matches from the search,
-        // including their `node`s, `treeIndex`es, and `path`s
-        // Here I just use it to note how many matches were found.
-        // This is optional, but without it, the only thing searches
-        // do natively is outline the matching nodes.
         searchFinishCallback={(matches) => {
           setSearchFoundCount(matches.length)
           setSearchFocusIndex(matches.length > 0 ? searchFocusIndex % matches.length : 0)
