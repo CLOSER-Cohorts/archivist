8fbe01809283773c79fc0cbb8475a060f839846b
Simon Reed <hello@simonreed.co>
2021-05-13 16:49:50 +0100

    Dataset view- cannot see the names of the variables mapped to derived variables #552

---
+++ public/precache-manifest.360ced3ce47003dea73813f051f1b3c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3e993531aeea3d959e96bad2662877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b20e7a96942f2b30c39e",
+    "url": "/static/css/2.a32671c1.chunk.css"
+  },
+  {
+    "revision": "b20e7a96942f2b30c39e",
+    "url": "/static/js/2.0a7d2b11.chunk.js"
+  },
+  {
+    "revision": "9d05258b8e28d03179dfee77ee91df96",
+    "url": "/static/js/2.0a7d2b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f25f5ffa0a68300e814",
+    "url": "/static/js/main.723b3e4f.chunk.js"
+  },
+  {
+    "revision": "9a036383e3b550c2c519",
+    "url": "/static/js/runtime-main.3adad9ea.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ad8f15cd47216a1e252ef8047e15243.js"
+  "/precache-manifest.360ced3ce47003dea73813f051f1b3c4.js"
 );
 
 self.addEventListener('message', (event) => {
