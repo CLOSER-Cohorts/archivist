50a8d5a486c90e9cba989a060d14628ec00b8ab8
Simon Reed <hello@simonreed.co>
2021-05-28 14:12:54 +0100

    Update assets

--- public/precache-manifest.fb034da13a8358eaaa563afcaecc2090.js
+++ public/precache-manifest.fb034da13a8358eaaa563afcaecc2090.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce327a0303ff4db37413a190914997b0",
+    "revision": "22d93bb6edc49099c1a9dff656ae3bfd",
     "url": "/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.4f78fa9f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd8fc8700b8ac16e2ac9",
-    "url": "/static/js/main.1aa47ac6.chunk.js"
+    "revision": "b1ea8a1c386f3330ba9c",
+    "url": "/static/js/main.e906122d.chunk.js"
   },
   {
     "revision": "9a036383e3b550c2c519",
---
+++ react/build/precache-manifest.fb034da13a8358eaaa563afcaecc2090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d93bb6edc49099c1a9dff656ae3bfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07845c6b918d7cf26fd",
+    "url": "/static/css/2.b58dcaa8.chunk.css"
+  },
+  {
+    "revision": "a07845c6b918d7cf26fd",
+    "url": "/static/js/2.4f78fa9f.chunk.js"
+  },
+  {
+    "revision": "9d05258b8e28d03179dfee77ee91df96",
+    "url": "/static/js/2.4f78fa9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ea8a1c386f3330ba9c",
+    "url": "/static/js/main.e906122d.chunk.js"
+  },
+  {
+    "revision": "9a036383e3b550c2c519",
+    "url": "/static/js/runtime-main.3adad9ea.js"
+  }
+]);
