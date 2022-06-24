668ebdb395f2fc2ef3b43883a5a8ced020e1d251
Simon Reed <hello@simonreed.co>
2022-06-24 12:07:20 +0100

    Use mailgun over sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['REACT_APP_API_HOST'] }
   config.asset_host = ENV['REACT_APP_API_HOST']
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['HOSTNAME'],
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
