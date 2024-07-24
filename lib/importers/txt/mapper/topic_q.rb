e0a1cf83974b6ea50c1941fa14e66037d2d86dc3
Simon Reed <hello@simonreed.co>
2024-07-24 10:16:01 +0100

    Fix topic q

--- lib/importers/txt/mapper/topic_q.rb
+++ lib/importers/txt/mapper/topic_q.rb
@@ -17,13 +17,17 @@ class Importers::TXT::Mapper::TopicQ < Importers::TXT::Mapper::Instrument
           @errors = true
           log :outcome, "Record Invalid as QuestionContruct and Topic where not found"
         else
-          qc.topic = topic
-          if qc.save
-            log :outcome, "Record Saved"
-            cc_question_ids_to_delete.delete(qc.id)
-          else
-            @errors = true
-            log :outcome, "Record Invalid : #{qc.errors.full_messages.to_sentence}"
+          ActiveRecord::Base.transaction do
+            qc.topic = topic
+          
+            if qc.save
+              log :outcome, "Record Saved"
+              cc_question_ids_to_delete.delete(qc.id)
+            else
+              @errors = true
+              log :outcome, "Record Invalid : #{qc.errors.full_messages.to_sentence}"
+              raise ActiveRecord::Rollback # Explicitly rollback the transaction
+            end
           end
         end
       rescue StandardError => e
