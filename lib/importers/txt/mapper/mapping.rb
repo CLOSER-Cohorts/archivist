1ae627a84d7d7b2f98317d156615d7ad368d5dc2
Simon Reed <hello@simonreed.co>
2026-04-21 10:59:27 +0100

    Flag partial QV imports as failure with per-question log entries (#887)

--- lib/importers/txt/mapper/mapping.rb
+++ lib/importers/txt/mapper/mapping.rb
@@ -9,8 +9,10 @@ class Importers::TXT::Mapper::Mapping < Importers::TXT::Mapper::Instrument
   def import(options = {})
     Map.where(id: @object.maps.pluck(:id)).delete_all
     set_import_to_running
+    @datasets_touched = []
 
     @doc.each do |control_construct_scheme, q, dataset, v|
+      @datasets_touched << dataset if dataset.present?
       log :input, "#{control_construct_scheme},#{q},#{dataset},#{v}"
       begin
         if control_construct_scheme.blank? || dataset.blank? || q.blank? || v.blank?
@@ -59,6 +61,38 @@ class Importers::TXT::Mapper::Mapping < Importers::TXT::Mapper::Instrument
         write_to_log
       end
     end
+
+    check_completeness
     set_import_to_finished
+  end
+
+  private
+
+  def check_completeness
+    return unless @import
+
+    unique_datasets = @datasets_touched.uniq.compact
+    all_instrument_datasets = @object.datasets.to_a
+    referenced_datasets = if unique_datasets.empty?
+      all_instrument_datasets
+    else
+      all_instrument_datasets.select { |ds| unique_datasets.include?(ds.instance_name) }
+    end
+
+    return if referenced_datasets.empty?
+
+    dataset_ids = referenced_datasets.map(&:id)
+    dataset_names = referenced_datasets.map(&:instance_name).to_sentence
+
+    @object.cc_questions.each do |cc_question|
+      next if cc_question.maps.joins(:variable).where(variables: { dataset_id: dataset_ids }).exists?
+
+      @errors = true
+      log :input, "#{@object.control_construct_scheme},#{cc_question.label},?,?"
+      log :outcome, I18n.t('importers.txt.mapper.mapping.question_not_mapped',
+                           question_label: cc_question.label,
+                           datasets: dataset_names)
+      write_to_log
+    end
   end
 end
